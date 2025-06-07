f048e458e9f9f02cc43444354228a4c33e96579a
Nicolas Moura <nicolasmouraignacio@outlook.com>
2025-06-07 14:09:03 -0300

    update

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-
 public class ContaBanco {
     public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+
     ContaTerminal contaTerminal = new ContaTerminal();
 
     contaTerminal.nomeCliente();
     contaTerminal.agencia();
     contaTerminal.numeroConta();
+    contaTerminal.saldo();
     contaTerminal.mensagem();
-
-
 
     sc.close();
 
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,23 +4,25 @@ public class ContaTerminal {
     int numeroConta;
     String agencia;
     String cliente;
-    double saldo= 1500;
+    double saldo;
 
     Scanner sc = new Scanner(System.in);
+
     public void nomeCliente(){
         System.out.println("Por favor, digite o seu nome completo: ");
         cliente = sc.nextLine();
-
     }
     public void agencia(){
         System.out.println("Por favor, digite o numero da agencia: ");
         agencia = sc.nextLine();
-
     }
     public void numeroConta(){
         System.out.println("Por favor, digite o numero da conta: ");
         numeroConta = sc.nextInt();
-
+    }
+    public void saldo(){
+        System.out.println("Por favor, digite o seu saldo: ");
+        saldo = sc.nextDouble();
     }
 
     public void mensagem(){
